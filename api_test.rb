72356726b3504641cce3843487c2bb20c301d979
Alex Desjardins <ardesjardins91@gmail.com>
2021-04-29 08:17:52 -0500

    Test failed response for missing api key.

--- api_test.rb
+++ api_test.rb
@@ -14,4 +14,14 @@ class APITest < Minitest::Test
     assert_equal to_json[:Search].class, Array
     assert_equal to_json[:Response], "True"
   end
+
+  def test_missing_api_key
+    url = 'http://www.omdbapi.com/?apikey='
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    to_json = JSON.parse(response, symbolize_names: true)
+
+    assert_equal to_json[:Response], "False"
+    assert_equal to_json[:Error], "No API key provided."
+  end
 end
